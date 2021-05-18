f7365f7a5e553c1f8d0a6db00bd38cc4a9aee16c
toesbieya <1647775459@126.com>
2021-05-18 08:51:32 +0800

    AmountInput增加maxlength、decimalLength属性

--- src/AmountInput/index.vue
+++ src/AmountInput/index.vue
@@ -2,6 +2,7 @@
   <div>
     <!--  用于输入的输入框  -->
     <van-field
+        ref="field"
         :label="label"
         :value="isFocused ? value : formattedValue"
         :type="isFocused ? 'number' : 'text'"
@@ -10,6 +11,8 @@
         :required="required"
         :rules="rules"
         :input-align="inputAlign"
+        :maxlength="maxlength"
+        :formatter="fieldFormatter"
         @input="onInput"
         @focus="onFocus"
         @blur="onBlur"
@@ -17,6 +20,9 @@
 
     <!--  用于展示大写金额的文本框  -->
     <van-field v-if="showCapital" label="大写" :value="uppercaseChinese" readonly :input-align="inputAlign"/>
+
+    <!--  避免不显示下边框  -->
+    <div class="van-cell" style="display: none"/>
   </div>
 </template>
 
@@ -40,6 +46,9 @@ export default {
     required: Boolean,
     rules: Array,
     inputAlign: String,
+    maxlength: {type: Number, default: 15},
+    // 小数点位数最多几位，为0时不限制
+    decimalLength: {type: Number, default: 2},
     // 是否显示下方的大写文本框
     showCapital: Boolean
   },
@@ -71,6 +80,20 @@ export default {
     },
     onBlur() {
       this.isFocused = false
+    },
+    // 在输入时限制小数点位数
+    fieldFormatter(v) {
+      if (isEmpty(v) || !this.decimalLength) return v
+
+      const i = v.indexOf('.')
+      if (i === -1) return v
+
+      if (i !== -1 && v.length - 1 - i >= this.decimalLength) {
+        const pair = v.split('.')
+        return `${pair[0]}.${pair[1].slice(0, this.decimalLength)}`
+      }
+
+      return v
     }
   }
 }
